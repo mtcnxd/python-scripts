1223b766333339b43080955e7bbe83c9d302b7ce
mtcnxd <mtc.nxd@gmail.com>
2026-07-22 22:14:16 -0600

    Led blink tested

--- raspberry-pi/gpio-control.py
+++ raspberry-pi/gpio-control.py
@@ -3,10 +3,14 @@ from services import BoardService
 board_service = BoardService()
 
 try:
-	while timer < 10:
-		print(f"Counter: {timer}")
-		timer = timer + 1
-		board_service.blink()
+    timer = 1
+    while timer <= 25:
+        print(f"Blinking: {timer}")
+        timer = timer + 1
+        board_service.blink()
 
 except Exception as error:
-	print(f"An error ocurred: {error}")
+    print(f"An error ocurred: {error}")
+
+finally:
+    board_service.cleanup()
--- raspberry-pi/services/BoardService.py
+++ raspberry-pi/services/BoardService.py
@@ -3,11 +3,17 @@ import time
 
 class BoardService:
     def __init__(self):
-        self.ledPin = 18
-        pass
+	# Select the fisical pin 12 (GPIO18)
+        self.ledPin = 12
+	# Configure the pinout as fisical pin numbers
+        gpio.setmode(gpio.BOARD)
+        gpio.setup(self.ledPin, gpio.OUT)
 
     def blink(self):
         gpio.output(self.ledPin, True)
-        time.sleep(0.5)
+        time.sleep(0.06)
         gpio.output(self.ledPin, False)
-        time.sleep(0.5)
+        time.sleep(0.12)
+
+    def cleanup(self):
+        gpio.cleanup()
