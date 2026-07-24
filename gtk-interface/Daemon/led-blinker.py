28917b36a520018f7e55cd7a27d66a2b7e1bf35b
mtcnxd <mtc.nxd@gmail.com>
2026-07-24 01:15:55 -0600

    Comments in README

--- gtk-interface/Daemon/led-blinker.py
+++ gtk-interface/Daemon/led-blinker.py
@@ -3,32 +3,33 @@ import time
 
 class BoardService:
     def __init__(self):
-	# Select the fisical pin 12 (GPIO18)
+        # Select the fisical pin 12 (GPIO18)
         self.ledPin = 12
-	# Configure the pinout as fisical pin numbers
+        
+        # Configure the pinout as fisical pin numbers
         gpio.setmode(gpio.BOARD)
         gpio.setup(self.ledPin, gpio.OUT)
 
     def blink(self):
         gpio.output(self.ledPin, True)
-        time.sleep(0.06)
+        time.sleep(0.10)
         gpio.output(self.ledPin, False)
-        time.sleep(0.12)
+        time.sleep(0.15)
 
     def cleanup(self):
         gpio.cleanup()
 
 
-while True:
-    try:
+try:
+    while True:
         board_service = BoardService()
         board_service.blink()
-        time.sleep(10)
+        time.sleep(3)
 
-    except Exception as error:
-        print(f"ERROR: {error}")
-        board_service.cleanup()
+except Exception as error:
+    print(f"ERROR: {error}")
+    board_service.cleanup()
 
-    finally:
-        board_service.cleanup()
+finally:
+    board_service.cleanup()
         
