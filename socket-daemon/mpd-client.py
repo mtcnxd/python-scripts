3c622d76c23387b63437a013817f57a12f9a887b
mtcnxd <mtc.nxd@gmail.com>
2026-08-12 15:30:45 -0600

    MPD client

--- socket-daemon/mpd-client.py
+++ socket-daemon/mpd-client.py
@@ -1,24 +1,48 @@
 import socket
-import time
+from time import sleep
 
 HOST = '127.0.0.1'
 PORT = 6600
 
-counter = 0
+def mpd_send_command(client, command):
+    client.sendall(f"{command}\n").encode()
+
+    response = b''
+
+    while True:
+        data = client.recv(4096)
+        response += data
+
+        if b'\nOK\n' in response:
+            break
+
+        return response.decode()
+
+def mpd_parse_response(response):
+    data = {}
+    for line in response.splitlines():
+        if ':' in line:
+            key, value = line.split(':', 1)
+            data[key] = value.strip()
+
+    return data
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
     try:
         client.connect((HOST, PORT))
-        
-        while counter < 20:
-            counter +=1
-            message = f"counter: {counter}"
-            client.sendall(message.encode())
-        
-            response = client.recv(1024)
-            print(f"Received: {response.decode()}")
-            
-            time.sleep(0.5)
+
+        while True:
+            client.sendall(b'currentsong\n')
+            response = client.recv(1024).decode()
+
+            current_track = mpd_parse_response(response)
+
+            print(f"Artista: {current_track.get('Artist', 'Desconocido')}")
+            print(f"Album: {current_track.get('Album', 'Desconocido')}")
+            print(f"Titulo: {current_track.get('Title', 'Desconocido')}")
+            print("="*30)
+            sleep(1)
 
     except Exception as error:
         print(error)
