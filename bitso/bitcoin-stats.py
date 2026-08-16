5144708d3c6afcd5c7fc4d7bae9dbbac3ba0c575
mtcnxd <mtc.nxd@gmail.com>
2026-08-15 20:34:37 -0600

    Add table

--- bitso/bitcoin-stats.py
+++ bitso/bitcoin-stats.py
@@ -8,11 +8,12 @@ from rich.table import Table
 
 bitso = Bitso()
 datalog = Datalog('backup.log')
+
 console = Console()
 
 def to_currency(value: str) -> str:
 	converted = float(value)
-	return f"$ {converted:,.2f}"
+	return f"${converted:,.2f}"
 
 def to_percentage(value):
 	converted = int(value)
@@ -20,16 +21,25 @@ def to_percentage(value):
 
 if __name__ == "__main__":
 	try:
-		current_time = datetime.now().strftime("%d-%m-%Y - %H:%M:%S")
+		current_time = datetime.now().strftime("%d-%m-%Y / %H:%M:%S")
 
 		book_info = bitso.get_book_info("btc_usdt")
 
 		datalog.write(f"{current_time} => Current price: {book_info['last']}")
 
-		console.print(f"Current date time:    {current_time}")
-		console.print(f"Bitcoin price:	      {to_currency(book_info['last'])}")
-		console.print(f"Change last hour:     {book_info['last']}")
-		console.print(f"Change last 24 hours: {book_info['last']}")
+		price_formatted = to_currency(book_info['last'])
+
+		table = Table()
+
+		table.add_column("key", style="dim")
+		table.add_column("value")
+
+		table.add_row("Date time", current_time)
+		table.add_row("Bitcoin price", price_formatted, style="bold blue")
+		table.add_row("Change last hour", price_formatted)
+		table.add_row("Change last 24 hour", price_formatted)
+
+		console.print(table)
 
 	except ApiException as error:
 		console.print(f"API Error: {error}")
